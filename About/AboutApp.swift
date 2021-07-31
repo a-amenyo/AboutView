5e15fd123f1e0c05941db32904e4f8c9f9cb64ff
a-amenyo <agbesi.amenyo@amalitech.org>
2021-07-31 01:03:38 +0000

    Initial Commit

---
+++ About/AboutApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutApp.swift
+//  About
+//
+//  Created by Mawuli Amenyo on 31/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct AboutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
