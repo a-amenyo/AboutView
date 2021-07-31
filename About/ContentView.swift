5c6c06a6b0131a8f3a10419d24a6e27246319744
a-amenyo <agbesi.amenyo@amalitech.org>
2021-07-31 09:44:33 +0000

    initial commit

--- About/ContentView.swift
+++ About/ContentView.swift
@@ -9,10 +9,53 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+       
+            NavigationView {
+                ScrollView{
+
+                VStack (alignment : .leading, spacing : 10){
+
+                    Section{
+                        
+                        Text("Privacy Policy")
+                            .foregroundColor(.black)
+                            .fontWeight(.heavy)
+                            .font(.title)
+                        
+                        Text("Last updated 27/07/021")
+                            .foregroundColor(.black)
+                            .font(.caption)
+                            .fontWeight(.light)
+                        Divider()
+                    }
+                 
+            
+                    Group{
+                        
+                    Text("Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem nemo, eum neque veritatis odio minima ad recusandae facere! Perferendis doloribus porro et amet dolores earum odio sunt, exercitationem quae aut?  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem nemo, eum neque veritatis odio minima ad recusandae facere! Perferendis doloribus porro et amet dolores earum odio sunt, exercitationem quae aut? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem nemo, eum neque veritatis odio minima ad recusandae facere! Perferendis doloribus porro et amet dolores earum odio sunt, exercitationem quae aut? " )
+                    
+                    Text("Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem nemo, eum neque veritatis odio minima ad recusandae facere! Perferendis doloribus porro et amet dolores earum odio sunt, exercitationem quae aut? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem nemo, eum neque veritatis odio minima ad recusandae facere! Perferendis doloribus porro et amet dolores earum odio sunt, exercitationem quae aut? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem nemo, eum neque veritatis odio minima ad recusandae facere! Perferendis doloribus porro et amet dolores earum odio sunt, exercitationem quae aut?")
+                    
+                    Text("Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem nemo, eum neque veritatis odio minima ad recusandae facere! Perferendis doloribus porro et amet dolores earum odio sunt, exercitationem quae aut?")
+                        
+                        Spacer()
+                    }
+                   
+                }
+                .toolbar(content: {
+                    ToolbarItem (placement: .navigation)  {
+                       Image(systemName: "arrow.left")
+                       .foregroundColor(.black)
+                    }
+                 })
+                .navigationBarTitle("Terms of Use", displayMode: .inline)
+                .padding()
+            }
+        }
+
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
